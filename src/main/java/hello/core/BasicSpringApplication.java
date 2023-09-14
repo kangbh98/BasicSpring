5be5d692948824d3f60ef45dbe8b694b1389441d
BH kang <rkdqudgjs1127@gmail.com>
2023-09-14 17:51:43 +0900

    김영한님의 스프링 핵심원리 기본편 예제 코드이다.
    순수한 자바 코드로 간단하게 만든 상품주문 예제코드이다

---
+++ src/main/java/hello/core/BasicSpringApplication.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BasicSpringApplication.class, args);
+    }
+
+}
