601df5df905ddffecd492f8cd0bb977091b194c9
BH kang <rkdqudgjs1127@gmail.com>
2023-09-16 19:07:36 +0900

    스프링 핵심 원리 이해2 - 객체 지향 원리 적용
    김영한님의 스프링 핵심원리 기본편 예제 코드이다.
    
    순수한 자바 코드로 간단하게 만든 상품주문 예제코드이다

--- src/main/java/hello/core/member/Grade.java
+++ src/main/java/hello/core/member/Grade.java
@@ -1,5 +1,5 @@
 package hello.core.member;
 
 public enum Grade {
-        VIP;
+        VIP,Basic
 }
--- src/main/java/hello/core/member/MemberServiceimpl.java
+++ src/main/java/hello/core/member/MemberServiceimpl.java
@@ -2,7 +2,12 @@ package hello.core.member;
 
 public class MemberServiceimpl implements MemberService{
 
-    private  final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceimpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
